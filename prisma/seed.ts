352073f2f4db876af1e27566cba900d8ca240355
BarbDMC <barbaradmorantesc@gmail.com>
2024-05-12 18:30:25 -0500

    build(Prisma): Add role field to user table
    property role was added to user model to define admin and client profiles

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,46 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      idUser: 1,
-      email: 'admin@example.com',
-      name: 'Admin',
-      surname: 'Admin',
-      password: '$2a$12$68/zZLfnwN3Yxn.OvMO7YumW4mj/mrVfIkoO34MOrs39rVHVQRDV6',
-      birthday: new Date('1990-01-01')
-    },
+  await prisma.user.createMany({
+    data: [
+      {
+        idUser: 1,
+        email: 'admin@example.com',
+        name: 'Admin',
+        surname: 'User',
+        password: '$2a$12$68/zZLfnwN3Yxn.OvMO7YumW4mj/mrVfIkoO34MOrs39rVHVQRDV6',
+        birthday: new Date('1990-01-01'),
+        role: 0
+      },
+      {
+        idUser: 2,
+        email: 'client@example.com',
+        name: 'Client',
+        surname: 'User',
+        password: '$2a$12$68/zZLfnwN3Yxn.OvMO7YumW4mj/mrVfIkoO34MOrs39rVHVQRDV6',
+        birthday: new Date('1990-01-01'),
+        role: 1
+      }
+    ]
   });
 
-  
+  const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${process.env.NYTIMES_API_KEY}`);
+  const data = await response.json();
+
+  const books = data.results.lists.flatMap((list: any) => {
+    return list.books.map((book: any) => ({
+      title: book.title,
+      author: book.author,
+      description: book.description,
+      price: Math.floor(Math.random() * 81) + 20,
+      inventory: Math.floor(Math.random() * 20),
+      image: book.book_image
+    }));
+  });
+
+  await prisma.book.createMany({
+    data: books
+  });
 }
 
 main()
