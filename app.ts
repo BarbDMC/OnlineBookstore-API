64e25396e624a5e7362a9217dc2fb9e937b0dcb7
BarbDMC <barbaradmorantesc@gmail.com>
2024-05-12 21:14:42 -0500

    build:

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import loginRoutes from './routes/loginRoutes';
 import booksRoutes from './routes/booksRoutes';
+import cors from "cors";
+import corsConfig from './config/cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors(corsConfig));
 app.use(loginRoutes);
 app.use(booksRoutes);
 app.listen(port);
